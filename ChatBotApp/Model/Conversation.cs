cd304165d873c861b5d443cc39e1e22e7244b5cf
Mohamed Fehmi <mohamed.fehmi@actiworks.de>
2020-01-12 15:10:38 +0100

    Added service Helper

---
+++ ChatBotApp/Model/Conversation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ChatBotApp.Model
+{
+    public class Conversation
+    {
+        public string ConversationId { get; set; }
+
+        public string Token { get; set; }
+
+        public string StreamUrl { get; set; }
+
+        public string ReferenceGrammarId { get; set; }
+
+        public int Expires_in { get; set; }
+    }
+}
---
+++ ChatBotApp/ViewModel/Helpers/BotServiceHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using ChatBotApp.Model;
+
+namespace ChatBotApp.ViewModel.Helpers
+{
+    public class BotServiceHelper
+    {
+        public BotServiceHelper()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private async System.Threading.Tasks.Task CreateConversationAsync()
+        {
+            string endpoint = "/qnamaker/v4.0";
+
+            using HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://experimentalbotqna.cognitiveservices.azure.com");
+            client.DefaultRequestHeaders.Add("Authorization", "Bearer 5_H3d9CT8JY.3IdaA1U9fh8OtDswkowQ69kXDrBeYA1YceLrcUfbT3g");
+
+            var response = await client.PostAsync(endpoint, null);
+
+            string json = await response.Content.ReadAsStringAsync();
+        }
+    }
+}
