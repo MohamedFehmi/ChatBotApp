17f305312d2e63112c8317e9ea7d72f0305d5efc
Mohamed Fehmi <mohamed.fehmi@actiworks.de>
2020-01-12 15:31:03 +0100

    Reconfigure helper with correct params

--- ChatBotApp/ViewModel/Helpers/BotServiceHelper.cs
+++ ChatBotApp/ViewModel/Helpers/BotServiceHelper.cs
@@ -1,29 +1,36 @@
 ﻿using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 using ChatBotApp.Model;
+using Newtonsoft.Json;
 
 namespace ChatBotApp.ViewModel.Helpers
 {
     public class BotServiceHelper
     {
+        public Conversation _Conversation { get; set; }
+
         public BotServiceHelper()
         {
+            CreateConversationAsync();
         }
 
         /// <summary>
-        /// 
+        /// Create a conversation
         /// </summary>
-        private async System.Threading.Tasks.Task CreateConversationAsync()
+        private async Task CreateConversationAsync()
         {
-            string endpoint = "/qnamaker/v4.0";
+            string endpoint = "/v3/directline/conversations";
 
             using HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("https://experimentalbotqna.cognitiveservices.azure.com");
+            client.BaseAddress = new Uri("https://directline.botframework.com");
             client.DefaultRequestHeaders.Add("Authorization", "Bearer 5_H3d9CT8JY.3IdaA1U9fh8OtDswkowQ69kXDrBeYA1YceLrcUfbT3g");
 
             var response = await client.PostAsync(endpoint, null);
 
             string json = await response.Content.ReadAsStringAsync();
+
+            _Conversation = JsonConvert.DeserializeObject<Conversation>(json);
         }
     }
 }
---
+++ ChatBotApp/ViewModel/MainVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ChatBotApp.ViewModel.Helpers;
+
+namespace ChatBotApp.ViewModel
+{
+    public class MainVM
+    {
+        BotServiceHelper botServiceHelper;
+
+        public MainVM()
+        {
+            botServiceHelper = new BotServiceHelper();
+        }
+    }
+}
