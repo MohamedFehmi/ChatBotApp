d0e12037273f94308c47ff4d613ae4df7f0ecd89
Mohamed Fehmi <mohamed.fehmi@actiworks.de>
2020-01-17 22:14:13 +0100

    Added BotsResponse model

---
+++ ChatBotApp/Model/BotsResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChatBotApp.Model
+{
+    public class BotsResponse
+    {
+		public string Watermark { get; set; }
+        public List<Activity> Activities { get; set; }
+    }
+}
