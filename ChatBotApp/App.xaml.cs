4031dd42ae371cef5db7af52d0e7383a3211aa70
Mohamed Fehmi <mohamed.fehmi@actiworks.de>
2020-01-18 09:56:48 +0100

    Added support for appcenter crash report and analytics

--- ChatBotApp/App.xaml.cs
+++ ChatBotApp/App.xaml.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using ChatBotApp.Helpers;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -6,15 +10,33 @@ namespace ChatBotApp
 {
     public partial class App : Application
     {
+        public static AppCenterHelper appCenterHelper;
+
         public App()
         {
             InitializeComponent();
 
+            if (appCenterHelper == null)
+                appCenterHelper = new AppCenterHelper();
+
             MainPage = new MainPage();
         }
 
-        protected override void OnStart()
+        protected override async void OnStart()
         {
+            //App secrets from AppCenter
+            string androidAppSecret = "9a1c4006-d975-4786-80b1-0ce86fc419dd";
+            string iOSAppSecret = "029038ae-6117-4e21-89c7-8fd71573d27d";
+
+            //Set-up the crash Service
+            AppCenter.Start($"android={androidAppSecret};ios={iOSAppSecret}", typeof(Crashes), typeof(Analytics));
+
+            bool didAppCrashed = await Crashes.HasCrashedInLastSessionAsync();
+            if (didAppCrashed)
+            {
+                var crashReport = await Crashes.GetLastSessionCrashReportAsync();
+                await Current.MainPage.DisplayAlert("Report", "A Crash Report was sent to the developer.", "OK");
+            }
         }
 
         protected override void OnSleep()
---
+++ ChatBotApp/Helpers/AppCenterHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace ChatBotApp.Helpers
+{
+    public class AppCenterHelper
+    {
+        public async Task TrackEvent(Dictionary<string, string> properties)
+        {
+            if (await Analytics.IsEnabledAsync())
+                Analytics.TrackEvent("Message_Sent", properties);
+        }
+
+        public async Task TrackError(Exception ex, Dictionary<string, string> properties)
+        {
+            if (await Crashes.IsEnabledAsync())
+                Crashes.TrackError(ex, properties);
+        }
+    }
+}
