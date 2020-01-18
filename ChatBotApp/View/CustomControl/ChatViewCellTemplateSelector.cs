23989aec87ed0cab38242764fa7dfcb0088386ca
Mohamed Fehmi <mohamed.fehmi@actiworks.de>
2020-01-18 08:17:17 +0100

    Added DataTemplateSelctor for incoming outgoing view cell

---
+++ ChatBotApp/View/CustomControl/ChatViewCellTemplateSelector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ChatBotApp.Model;
+using Xamarin.Forms;
+
+namespace ChatBotApp.View.CustomControl
+{
+    public class ChatViewCellTemplateSelector : DataTemplateSelector
+    {
+        private readonly DataTemplate incomingDataTemplate;
+        private readonly DataTemplate outgoingDataTemplate;
+
+        public ChatViewCellTemplateSelector()
+        {
+            incomingDataTemplate = new DataTemplate(typeof(IncomingViewCell));
+            outgoingDataTemplate = new DataTemplate(typeof(OutgoingViewCell));
+        }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var chatMessage = item as ChatMessage;
+
+            if (chatMessage != null)
+            {
+                return chatMessage.IsInComing ? incomingDataTemplate : outgoingDataTemplate;
+            }
+            return null;
+        }
+    }
+}
